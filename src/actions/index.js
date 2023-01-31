5cdd3d7949dfc97541fc6d10127f50daf333d38e
Kelsey Jackson <kelseythej@gmail.com>
2023-01-31 00:49:52 -0600

    reducing api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch) => {
+  await dispatch(fetchPosts());
+};
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
